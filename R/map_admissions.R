27fd4e448d84ecff870e82fd8ca597001db1d6f9
seabbs <s.e.abbott12@gmail.com>
2021-01-19 14:13:46 +0000

    tweak NA colour and add background shapefile plotting

--- R/map_admissions.R
+++ R/map_admissions.R
@@ -6,7 +6,8 @@
 #' Must contain the following variables: `geo_code`, `date`, and `admissions`
 #' @param date A date variable (or vector) indicating when to plot data for. 
 #' @param scale_fill A `ggplot2` `scale_fill` used to define the fill colours 
-#' used in the map. The default is: `scale_fill_viridis_c(option = "viridis", direction = -1)`.
+#' used in the map. The default is: 
+#' `scale_fill_viridis_c(option = "viridis", direction = -1, na.value = "grey80")`.
 #' @inheritParams summarise_mapping
 #' @importFrom ggplot2 ggplot geom_sf aes scale_fill_viridis_c labs theme_void theme guides guide_colorbar
 #' @importFrom dplyr filter
@@ -22,7 +23,8 @@ map_admissions <- function(admissions, shapefile, date, scale_fill) {
   }
   
   if (missing(scale_fill)) {
-    scale_fill <- scale_fill_viridis_c(option = "viridis", direction = -1)
+    scale_fill <- scale_fill_viridis_c(option = "viridis", direction = -1, 
+                                       na.value = "grey80")
   }
   
   admissions <- admissions %>% 
@@ -31,6 +33,7 @@ map_admissions <- function(admissions, shapefile, date, scale_fill) {
   g <- shapefile %>%
     left_join(admissions, by = "geo_code") %>% 
     ggplot() +
+    geom_sf(data = shapefile, lwd = 0.3, col = "grey60") +
     geom_sf(aes(fill = admissions), lwd = 0.3, col = "grey20") +
     scale_fill +
     theme_void() +
