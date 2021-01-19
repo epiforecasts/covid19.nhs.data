4f7bec9946249f669a01f6d83428ca842d26d859
sophiemeakin <sophiemeakin@outlook.com>
2021-01-19 13:30:27 +0000

    Update england_ltla_shape
    * Shapefile from May 2020
    * Includes merger of E07000004, E07000005, E07000006, E07000007 to create E06000060

--- data-raw/england_ltla_shape.R
+++ data-raw/england_ltla_shape.R
@@ -7,10 +7,10 @@ library(stringr)
 england_ltla_shape <- sf::read_sf(here::here("data-raw", "raw", "uk_ltla_shp", "ltla_uk.shp")) %>%
   sf::st_transform(27700) %>%
   sf::st_simplify(dTolerance = 100) %>%
-  dplyr::filter(str_starts(lad19cd, "E")) %>%
+  dplyr::filter(str_starts(LAD20CD, "E")) %>%
   dplyr::rename(
-    geo_code = lad19cd,
-    geo_name = lad19nm
+    geo_code = LAD20CD,
+    geo_name = LAD20NM
   )
 
 usethis::use_data(england_ltla_shape, overwrite = TRUE)
