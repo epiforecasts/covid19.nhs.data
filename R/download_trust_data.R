12ec56da99b06387a14a1d83320c53b5e4918730
sophiemeakin <sophiemeakin@outlook.com>
2021-04-30 12:51:18 +0100

    Update download_trust_data
    * Data is split between two files (until 06 April, from 07 April 2021)
    * Update downloads, reshapes and combines two files; output format has not changed

--- R/download_trust_data.R
+++ R/download_trust_data.R
@@ -21,8 +21,23 @@ download_trust_data <- function(release_date = Sys.Date()) {
   ## Revert to the last Thursday
   release_date <- as_date(release_date)
   release_date <- floor_date(release_date, unit = "week", week_start = 4)
+  
+  # Data until 06 April 2021 (inclusive)
+  nhs_past_url <- paste0(
+    "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/",
+    year(release_date), "/",
+    ifelse(month(release_date) < 10,
+           paste0(0, month(release_date)),
+           month(release_date)
+    ),
+    "/Weekly-covid-admissions-and-beds-publication-",
+    gsub("-", "", as.character(format.Date(release_date, format = "%y-%m-%d"))),
+    "-up-to-210406",
+    ".xlsx"
+  )
 
-  nhs_url <- paste0(
+  # Data from 07 April 2021 (inclusive)
+  nhs_current_url <- paste0(
     "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/",
     year(release_date), "/",
     ifelse(month(release_date) < 10,
@@ -34,11 +49,11 @@ download_trust_data <- function(release_date = Sys.Date()) {
     ".xlsx"
   )
 
-  if (!url.exists(nhs_url)) {
+  if (!url.exists(nhs_current_url)) {
 
     ## Try last week data
     release_date <- release_date - 7
-    nhs_url <- paste0(
+    nhs_current_url <- paste0(
       "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/",
       year(release_date), "/",
       ifelse(month(release_date) < 10,
@@ -52,11 +67,12 @@ download_trust_data <- function(release_date = Sys.Date()) {
   }
 
   tmp <- file.path(tempdir(), "nhs.xlsx")
-  download.file(nhs_url, destfile = tmp, mode = "wb")
+  
+  # Download past data 
+  download.file(nhs_past_url, destfile = tmp, mode = "wb")
+  sheet_names <- setdiff(excel_sheets(tmp), c("Adult G&A Beds Unoccupied Non", "Adult CC Beds Unoccupied Non"))
 
-  sheet_names <- setdiff(excel_sheets(tmp), "Adult G&A Beds Unoccupied Non")
-
-  out <- map_df(.x = sheet_names, .f = ~ {
+  out_past <- map_df(.x = sheet_names, .f = ~ {
     dat <- suppressMessages(read_excel(tmp,
       sheet = .x,
       range = cell_limits(c(15, 1), c(522, NA))
@@ -66,7 +82,7 @@ download_trust_data <- function(release_date = Sys.Date()) {
     colnames(dat) <- c(
       colnames(dat)[1:4],
       as.character(seq.Date(
-        from = as.Date(ifelse(grepl("G&A", .x), "2020-11-17", "2020-08-01")),
+        from = as.Date(ifelse((grepl("G&A", .x) | grepl("CC", .x)), "2020-11-17", "2020-08-01")),
         by = "day", length = ncol(dat) - 4
       ))
     )
@@ -90,6 +106,48 @@ download_trust_data <- function(release_date = Sys.Date()) {
     return(out)
   }) %>%
     bind_rows()
+  
+  # Download recent data
+  download.file(nhs_current_url, destfile = tmp, mode = "wb")
+  sheet_names <- setdiff(excel_sheets(tmp), c("Adult G&A Beds Unoccupied Non", "Adult CC Beds Unoccupied Non"))
+  
+  out_recent <- map_df(.x = sheet_names, .f = ~ {
+    dat <- suppressMessages(read_excel(tmp,
+                                       sheet = .x,
+                                       range = cell_limits(c(15, 1), c(522, NA))
+    )) %>%
+      tibble()
+    
+    colnames(dat) <- c(
+      colnames(dat)[1:4],
+      as.character(seq.Date(
+        from = as.Date("2021-04-07"),
+        by = "day", length = ncol(dat) - 4
+      ))
+    )
+    
+    out <- dat %>%
+      pivot_longer(cols = -colnames(dat)[1:4], names_to = "date", values_to = "value") %>%
+      rename(
+        type1_acute = `Type 1 Acute?`,
+        nhs_region = `NHS England Region`,
+        org_code = Code,
+        org_name = Name
+      ) %>%
+      mutate(
+        type1_acute = ifelse(type1_acute == "Yes", TRUE, FALSE),
+        org_code = ifelse(org_code != "-", org_code, NA),
+        date = as.Date(date),
+        data = .x
+      ) %>%
+      filter(!is.na(org_code)) %>%
+      select(data, nhs_region, type1_acute, org_code, org_name, date, value)
+    return(out)
+  }) %>%
+    bind_rows()
+  
+  out <- out_past %>%
+    bind_rows(out_recent)
 
   return(out)
 }
