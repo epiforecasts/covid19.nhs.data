eee33a60dc3c2d3f3fe806b0031c8decd936d73f
Sophie Meakin <sophiemeakin@outlook.com>
2022-08-23 15:30:24 +0100

    Drop default mapping and geo_names for get_names()

--- R/get_names.R
+++ R/get_names.R
@@ -1,20 +1,28 @@
 #' Link Mapping to Trust and Geography Names
 #'
-#' @param mapping data.frame containing trust_code, p_trust, geo_code and p_geo. 
-#' Defaults to `trust_utla_mapping` if not supplied.
-#' @param geo_names A dataframe containing `geo_code` and `geo_name`. Used to 
+#' @param mapping A data.frame containing geo_code, trust_code, p_geo and p_trust.
+#' @param geo_names A data.frame containing `geo_code` and `geo_name`. Used to 
 #' assign meaningful to geographies.
 #' @return A data.frame containing a UTLA to trust level admissions map combined
 #' meaningful names.
 #' @export
 #' @importFrom dplyr left_join select
 get_names <- function(mapping, geo_names) {
-  if (missing(geo_names)) {
-    geo_names <- covid19.nhs.data::utla_names
+  
+  if (missing(mapping)) {
+    stop("Missing mapping - please specify a LTLA- or UTLA-Trust mapping.")
+  } else {
+    # Geography names checks
+    if (missing(geo_names)) {
+      stop("Missing geo_names - please specify geography names.")
+    }
   }
+  
   out <- mapping %>%
     left_join(covid19.nhs.data::trust_names, by = "trust_code") %>%
     left_join(geo_names, by = "geo_code") %>%
     select(trust_code, trust_name, geo_code, geo_name, p_trust, p_geo)
+  
   return(out)
+  
 }
