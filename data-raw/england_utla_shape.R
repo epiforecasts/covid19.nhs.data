75cf1a7a7af94cf9b8aca7a8b2e6113e1026c249
Sophie Meakin <sophiemeakin@outlook.com>
2022-09-22 15:49:25 +0100

    Fix R-CMD-check
    * Error: Error: R CMD check found WARNINGs
    * Warning: found non-ASCII strings (in england_ltla_shape and england_utla_shape)
    * Non-ASCII strings were degree symbol
    * Fixed by replacing them with the corresponding ASCII code (thanks @jengelaere https://github.com/r-spatial/sf/issues/1341)
    * Also replace library() with pacman::p_load()

--- data-raw/england_utla_shape.R
+++ data-raw/england_utla_shape.R
@@ -1,8 +1,10 @@
-library(sf)
-library(dplyr)
-library(usethis)
-library(here)
-library(stringr)
+pacman::p_load(
+  sf,
+  here,
+  dplyr,
+  stringr,
+  usethis
+)
 
 england_utla_shape <- sf::read_sf(here::here("data-raw", "raw", "uk_utla_shp", "utla_uk.shp")) %>%
   sf::st_transform(27700) %>%
@@ -13,5 +15,10 @@ england_utla_shape <- sf::read_sf(here::here("data-raw", "raw", "uk_utla_shp", "
     geo_name = ctyua19nm
   ) %>%
   dplyr::mutate(geo_code = ifelse(geo_code == "E10000002", "E06000060", geo_code))
+
+# Replace non-ASCII character (degree symbol) with corresponding ASCII code
+st_crs(england_utla_shape)$wkt <- gsub(pattern = "°",
+                                       replacement = "\\\u00b0",
+                                       x = st_crs(england_utla_shape)$wkt)
 
 usethis::use_data(england_utla_shape, overwrite = TRUE)
